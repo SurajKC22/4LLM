78385eb0e553e930fb755f133659df5abd364f3a
Surajkc22 <surajjkc09@gmail.com>
2024-10-11 11:51:59 +0930

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -2,38 +2,31 @@ import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-// Interface for the mongoose connection cache
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
-// Declare global type for mongoose
-declare global {
-  var mongoose: MongooseConnection; // This must be var and not let or const
+let cached: MongooseConnection = (global as any).mongoose
+
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-// Use global mongoose cache, or initialize it if it doesn't exist
-let cached: MongooseConnection = global.mongoose || { conn: null, promise: null };
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-export const connectToDatabase = async (): Promise<Mongoose> => {
-  // If a connection is already cached, return it
-  if (cached.conn) return cached.conn;
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: '4LLLM', bufferCommands: false 
+    })
 
-  // If MONGODB_URL is not set in the environment, throw an error
-  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-
-  // If no promise is cached, start a new connection promise
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { 
-    dbName: '4LLLM', bufferCommands: false 
-  });
-
-  // Wait for the connection promise to resolve, then cache the connection
   cached.conn = await cached.promise;
 
   return cached.conn;
-};
+}
